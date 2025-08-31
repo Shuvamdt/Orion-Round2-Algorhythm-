9cb9bdeaf9ed3b9a46329c872b0e2c6e8759683c
Shuvamdt <shuvamdt1230@gmail.com>
2025-08-31 13:56:51 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [menuOpen, setOpen] = useState(false);
+  const toggleMenu = () => {
+    setOpen(true);
+  };
   return (
     <div className="absolute alumni flex justify-between items-center top-0 px-20 py-10 w-full">
       <h1 className="flex flex-col">
@@ -24,6 +28,14 @@ const Navbar = () => {
       <div>
         <button className="px-4 py-3 jaro bg-[#F949C8] text-[15px] lg:text-[20px] text-white">
           REGISTER TODAY{" "}
+        </button>
+      </div>
+      <div className="flex lg:hidden">
+        <button
+          className=" px-4 py-3 jaro bg-[#F949C8] text-[15px] lg:text-[20px] text-white"
+          onClick={toggleMenu}
+        >
+          MENU
         </button>
       </div>
     </div>
